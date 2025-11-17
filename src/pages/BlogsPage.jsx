d6d9d0cbc8a927748928c0564b80ea6fc9056e11
Talha Ahmed <talha.codes970@gmail.com>
2025-11-17 21:39:35 +0530

    added contact and blogs page

--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+
 import img1 from "../assets/images/nojob.jpeg";
-import img3 from "../assets/images/Internship.jpeg";
+import img3 from "../assets/images/internship.jpeg";
 import img4 from "../assets/images/indemand.jpeg";
 import img5 from "../assets/images/degree.jpeg";
-import img6 from "../assets/images/Img6.jpeg";
-import img7 from "../assets/images/Img7.jpeg";
-import img8 from "../assets/images/Img8.jpeg";
-import img9 from "../assets/images/Img9.jpeg";
-import img10 from "../assets/images/Img10.jpeg";
+import img6 from "../assets/images/img6.jpeg";
+import img7 from "../assets/images/img7.jpeg";
+import img8 from "../assets/images/img8.jpeg";
+import img9 from "../assets/images/img9.jpeg";
+import img10 from "../assets/images/img10.jpeg";
 
 const BlogsData = [
   {
